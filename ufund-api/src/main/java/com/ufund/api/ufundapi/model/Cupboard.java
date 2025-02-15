2317308911d4032be060ba0a10ca78fc87bcb318
Sebastian Canakis <scanakis@Sebastians-MacBook-Pro-3.local>
2025-02-15 14:24:07 -0500

    Constructor and GetNeedByName for Cupboard

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Cupboard.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Cupboard.java
@@ -1,5 +1,35 @@
 package com.ufund.api.ufundapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Cupboard {
     
+
+    // ONlY ONE CUPBOARD
+    // CONTAINS AN ARRAY OF NEEDS
+    
+    @JsonProperty("needs") private Need[] needs;
+
+    public Cupboard(Need[] needs) {
+        this.needs = needs;
+    }
+
+    public Need getNeedByName(String needString) {
+
+        for(int i = 0; i < needs.length; i++) {
+            if(needs[i].getName() == needString) {
+                return needs[i];
+            }
+        } 
+
+        return null;
+
+
+    }
+
+
+
+
+
+
 }
