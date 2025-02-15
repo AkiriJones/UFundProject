ded3518cfd80d4b92d1147c4f2986230f014e333
Akiri Jones <aj1974@rit.edu>
2025-02-15 14:27:17 -0500

    Created Cupboard DAO interface

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardDAO.java
@@ -0,0 +1,19 @@
+package com.ufund.api.ufundapi.persistence;
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.model.*;
+
+public interface CupboardDAO {
+
+    Need[] getNeeds() throws IOException;
+
+    Need[] findNeeds(String containsString) throws IOException;
+
+    Need getNeed(String name) throws IOException;
+
+    Need createNeed(String name, int cost, int quantity, String type) throws IOException;
+
+    Need updateNeed(String name) throws IOException;
+
+    boolean deleteNeed(String name) throws IOException;
+}
