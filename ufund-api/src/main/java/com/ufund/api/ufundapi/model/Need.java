bd4721a061ef6cda09386ebaef3387adcc4769ce
Akiri Jones <aj1974@rit.edu>
2025-02-15 15:54:08 -0500

    Added documentation for Need.java

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -3,7 +3,11 @@ package com.ufund.api.ufundapi.model;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+/**
+ * Represents a Need entity
+ * 
+ * @author Akiri Jones, Jack Faro, Caden Estermann, Sebastian Canakis Diaz, Giulia Spier
+ */
 public class Need {
     private static final Logger LOG = Logger.getLogger(Need.class.getName());
 
@@ -13,19 +17,48 @@ public class Need {
     @JsonProperty("cost") private double cost;
     @JsonProperty("quantity")private int quantity;
     @JsonProperty("type") private String type;
-
+    /**
+     * Create a Need with a given name
+     * @param name The name of the need
+     * @param cost The cost of the need
+     * @param quantity The amount of said need present 
+     * @param type The type of need (Physical or Monetary)
+     */
     public Need(@JsonProperty("name") String name, @JsonProperty("cost") double cost, @JsonProperty("quantity") int quantity, @JsonProperty("type") String type){
         this.name = name;
         this.cost = cost;
         this.quantity = quantity;
         this.type = type;
     }
-
+    /**
+     * Retrieves the name of a need
+     * @return The name of the need
+     */
     public String getName() {return name;}
+    /**
+     * Sets the name of a need - neccessary for deserialization
+     * @param name The name of a need
+     */
     public void setName(String name) {this.name = name;}
+    /**
+     * Retrieves the cost of a need
+     * @return The cost of the need
+     */
     public Double getCost() {return cost;}
+    /**
+     * Sets the cost of a need - neccessary for deserialization
+     * @param cost the cost of a need
+     */
     public void setCost(double cost){this.cost = cost;}
+    /**
+     * Retrieves the quantity of a need
+     * @return The quantity of the need
+     */
     public int getQuantity() {return quantity;}
+    /**
+     * Sets the quantity of a - neccessary for deserialization
+     * @param quantity The quantity of the need
+     */
     public void setQuantity(int quantity) {this.quantity = quantity;}
     
 
