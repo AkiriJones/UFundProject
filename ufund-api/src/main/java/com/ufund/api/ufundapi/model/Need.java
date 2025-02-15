4463392f3aa3728975c002a12803c36cac35b2da
Caden Esterman <ffe5712@rit.edu>
2025-02-15 14:25:11 -0500

    Need class updated with methods

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -13,9 +13,33 @@ public class Need {
     @JsonProperty("cost") private double cost;
     @JsonProperty("quantity")private int quantity;
     @JsonProperty("type") private String type;
-    @JsonProperty("id") private int id;
 
-    public Need(@JsonProperty("name") String name, @JsonProperty("cost") double cost, @JsonProperty("quantity") int quantity, @JsonProperty("type") String type, @JsonProperty("id") int id){
-        
+    public Need(@JsonProperty("name") String name, @JsonProperty("cost") double cost, @JsonProperty("quantity") int quantity, @JsonProperty("type") String type){
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.type = type;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public Double getCost() {return cost;}
+    public void setCost(double cost){this.cost = cost;}
+    public int getQuantity() {return quantity;}
+    public void setQuantity(int quantity) {this.quantity = quantity;}
+    public String getType() {return type;}
+    public void setType(String type){this.type = type;}
+
+    @Override
+    public String toString(){
+        return String.format(STRING_FORMAT,name,cost,quantity,type);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Need need = (Need) obj;
+        return name.equals(need.name);
     }
 }
