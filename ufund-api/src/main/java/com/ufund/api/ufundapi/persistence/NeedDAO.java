4c29998ff839f07c0b38f4d214608b4b952e4d66
jnf8565 <jnf8565@rit.edu>
2025-02-15 14:30:09 -0500

    Created NeedDao interface 	modified:   ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
@@ -1,5 +1,17 @@
 package com.ufund.api.ufundapi.persistence;
 
-public class NeedDAO {
-    
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.model.Need;
+
+public interface NeedDAO {
+
+    Need createNeed(Need need) throws IOException;
+
+    Need getNeed(String name) throws IOException;
+
+    Need updateNeed(Need need) throws IOException;
+
+    boolean deleteNeed(String name) throws IOException;
+
 }
