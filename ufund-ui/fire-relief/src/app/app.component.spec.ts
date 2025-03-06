c7242cadb31fa89e4e617c82f56c1e1c77444b63
Caden Esterman <ffe5712@rit.edu>
2025-03-05 21:53:01 -0500

    remade angular workspace

--- ufund-ui/fire-relief/src/app/app.component.spec.ts
+++ ufund-ui/fire-relief/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'fire-relief' title`, () => {
+  it(`should have as title 'fire-relief'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('fire-relief');
