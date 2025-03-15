d861d69a80b6473c9b5afbea3a28d5d7195bb4be
Caden Esterman <ffe5712@rit.edu>
2025-03-14 22:12:12 -0400

    basket component initializing method

--- ufund-ui/fire-relief/src/app/Helper/basket/basket.component.ts
+++ ufund-ui/fire-relief/src/app/Helper/basket/basket.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Need } from '../../need';
+import { BasketService } from '../../basket.service';
+import { CupboardService } from '../../cupboard.service';
+import { UserService } from '../../user.service';
+import { Router } from '@angular/router';
 
+/**
+ * Component responsible for managing and displaying the user's basket.
+ */
 @Component({
   selector: 'app-basket',
   standalone: false,
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.css'
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit {
+  basketItems: { need: Need, quantity: number }[] = []
 
+  /**
+   * Constructs the BasketComponent.
+   * 
+   * @param basketService Service for managing basket operations.
+   * @param cupboardService Service for retrieving cupboard data.
+   * @param userService Service for managing user data.
+   * @param router Servuce to handle navigation.
+   */
+  constructor(
+    private basketService: BasketService,
+    private cupboardService: CupboardService,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  /**
+   * Lifecycle hook called when the component is initialized.
+   * Retrieves current user data based on username.
+   */
+  ngOnInit(): void {
+    const username = localStorage.getItem("username");
+
+    if(username) {
+      this.userService.getUser(username).subscribe(user => {
+        this.userService.user = user;
+
+      this.cupboardService.getNeedsFromBasket().subscribe(needs => {
+        this.basketItems = needs;
+      });
+      });
+    }
+    else {
+      console.log("No username found");
+    }
+  }
+
+  /**
+   * Navigates to cupboard
+   */
+  cupboardButton(): void {
+    this.router.navigate(['/cupboard']);
+  }
+
+  /**
+   * Navigates to basket
+   */
+  basketButton(): void {
+    this.router.navigate(['/basket']);
+  }
 }
