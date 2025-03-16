ea3bffa32f14b3f8674009704b863286a1369c60
Akiri Jones <aj1974@rit.edu>
2025-03-16 17:10:02 -0400

    Modified checkout to change cupboard correctly, and display total cost of checkout

--- ufund-ui/fire-relief/src/app/Helper/basket/basket.component.ts
+++ ufund-ui/fire-relief/src/app/Helper/basket/basket.component.ts
@@ -19,6 +19,7 @@ export class BasketComponent implements OnInit {
   searchTerm: string = '';
   needs: Need[] = [];
   allNeeds: Need[] = [];
+  totalCost: number = 0
   /**
    * Constructs the BasketComponent.
    * 
@@ -47,6 +48,7 @@ export class BasketComponent implements OnInit {
 
       this.cupboardService.getNeedsFromBasket().subscribe(needs => {
         this.basketItems = needs;
+        this.calculateTotalCost();
       });
       });
 
@@ -76,9 +78,11 @@ export class BasketComponent implements OnInit {
     if (!existingItem){
           this.basketService.addToBasket(need);
           this.basketItems.push({need, quantity: 1});
+          this.calculateTotalCost();
     }
     else{
       existingItem.quantity++;
+      this.calculateTotalCost();
     }
   }
 
@@ -88,13 +92,19 @@ export class BasketComponent implements OnInit {
         id: element.need.id,
         name: element.need.name,
         cost: element.need.cost,
-        quantity: element.need.quantity + element.quantity, 
+        quantity: element.need.quantity - element.quantity, 
         type: element.need.type}
         this.cupboardService.updateNeed(element.need.id, needContents).subscribe();
     });
     this.basketService.clearBasket();
+    this.calculateTotalCost();
     this.basketItems = [];
   }
+
+  calculateTotalCost(): void {
+    this.totalCost = this.basketItems.reduce((sum, item) => sum + (item.need.cost * item.quantity), 0);
+  }
+
   /**
    * Navigates to cupboard
    */
