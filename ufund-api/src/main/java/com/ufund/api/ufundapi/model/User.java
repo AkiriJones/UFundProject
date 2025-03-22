f4fd19341eb65ea7ac8db58346622428734492e2
DigitalCitys <giulia.spier@gmail.com>
2025-03-22 16:57:50 -0400

    Added transactionHistory as state to the user

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
@@ -9,6 +9,7 @@ public class User {
 
     @JsonProperty("name") String name;
     @JsonProperty("basket") Basket basket;
+    @JsonProperty("transactionHistory") Transaction[] transactionHistory;
 
     /**
      * Constructs a User with the specified username and basket.
@@ -16,9 +17,10 @@ public class User {
      * @param name the username of the user
      * @param basket the basket associated with the user; if null, a new Basket is created
      */
-    public User(@JsonProperty("name") String name, @JsonProperty("basket") Basket basket) {
+    public User(@JsonProperty("name") String name, @JsonProperty("basket") Basket basket, @JsonProperty("transactionHistory") Transaction[] transactionHistory) {
         this.name = name;
         this.basket = basket;
+        this.transactionHistory = transactionHistory;
 
         if(basket == null) {
             this.basket = new Basket();
@@ -41,5 +43,14 @@ public class User {
      */
     public Basket getBasket() {
         return basket;
+    }
+
+    /**
+     * Gets the user's transaction history.
+     *
+     * @return the user's transaction history.
+     */
+    public Transaction[] getTransactionHistory() {
+        return transactionHistory;
     }
 }
